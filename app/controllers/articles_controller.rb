57481153975e40632f5074c246dcac6421d5bbee
lushcodes <lushcodes@gmail.com>
2022-05-17 09:09:48 +0100

    add error handling for new article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,23 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #strong params required
+    @article = Article.new(params.require(:article).permit(:title, :description))
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
   end
 end
