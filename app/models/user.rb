087682ed1465a34d24946c4e45871b0fe86a4581
lushcodes <lushcodes@gmail.com>
2022-05-25 11:00:57 +0100

    Add password digest column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
 
+  # before saving record to the database
+  before_save { self.email = email.downcase! }
+
+  # associations
   has_many :articles
 
+  # validations
   validates :username, presence: true,
               uniqueness: { case_sensitive: false },
               length: { minimum: 3, maximum: 25 }
@@ -12,4 +17,6 @@ class User < ApplicationRecord
               uniqueness: { case_sensitive: false },
               length: { maximum: 105 },
               format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
