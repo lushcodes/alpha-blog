fe375024aa74ee71ceb4f9c6225f30e91c8f874c
lushcodes <lushcodes@gmail.com>
2022-05-25 14:44:31 +0100

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+
   resources :articles # You can expose selected routes using (only: [:show, :index, :new, :create, :edit, :update])
+  resources :users, except: [:new]
 end
