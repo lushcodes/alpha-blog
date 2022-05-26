c64db9857d2798f47078ff2c2137fbde09740787
lushcodes <lushcodes@gmail.com>
2022-05-26 14:01:27 +0100

    Add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   resources :articles # You can expose selected routes using (only: [:show, :index, :new, :create, :edit, :update])
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
