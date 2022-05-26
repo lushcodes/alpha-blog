5bb4a8edde030952ef6d101c7ec7e39df97248a8
lushcodes <lushcodes@gmail.com>
2022-05-26 15:57:39 +0100

    Restrict views based on login credentials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     #strong params required
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
